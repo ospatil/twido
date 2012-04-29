fb416fe4478cb654407e86019735aa54fff3aa73
Omkar Patil <ospatil@gmail.com>
2012-04-29 19:05:22 +0530

    Added db_string env variable

--- modules/conf.js
+++ modules/conf.js
@@ -2,6 +2,6 @@ module.exports = {
     tw_consumer_key: process.env['tw_consumer_key']
     , tw_consumer_secret: process.env['tw_consumer_secret']
     , session_secret: process.env['session_secret']
-    , db_string: 'mongodb://localhost/gnie'
+    , db_string: process.env['db_string']
     , app_port: process.env['app_port']
 };
